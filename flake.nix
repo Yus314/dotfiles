ad7122a8fddbf34765b8f24ebc3d51b69f2c89bc
柿沼祐介 <kakinumayuusuke@kakinumayuusukenoMacBook-Air.local>
2024-04-23 23:02:39 +0900

    darwin

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
 	description = "A simple NixOS flakes";
 	
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+		#nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 		unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+		#nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
 		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 		home-manager = {
 			url = "github:nix-community/home-manager/release-23.11";
@@ -13,12 +15,17 @@
 			url = "github:nix-community/nixvim/nixos-23.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
+		nix-darwin = {
+			url = "github:LnL7/nix-darwin";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 		
 	};
 	outputs = inputs: {
 		nixosConfigurations = {
 			myNixOS = inputs.nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
+				#system = "x86_64-linux";
+				system = " aarch64-darwin";
 				modules = [
 					./configuration.nix
 				];
@@ -27,7 +34,8 @@
 		homeConfigurations = {
 			myHome = inputs.home-manager.lib.homeManagerConfiguration {
 				pkgs = import inputs.nixpkgs {
-					system = "x86_64-linux";
+					#system = "x86_64-linux";
+					system = " aarch64-darwin";
 					config.allowUnfree = true;
 				};
 				extraSpecialArgs = {
@@ -37,6 +45,9 @@
 					./home.nix
 				];
 			};
+		};
+		darwinConfigurations."kakinumayuusukenoMacBook-Air" = inputs.nix-darwin.lib.darwinSystem {
+			modules = [./darwin-configuration.nix ];
 		};
 	};
 
