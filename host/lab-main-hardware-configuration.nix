e079e59bf35b7367b4b8ecf1f0582480ce9fafa7
Yus314 <shizhaoyoujie@gmail.com>
2024-07-07 19:05:54 +0900

    cancel main host file

--- host/lab-main-hardware-configuration.nix
+++ host/lab-main-hardware-configuration.nix
@@ -8,24 +8,30 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
+    { device = "/dev/disk/by-uuid/66012d3f-bde2-478b-b384-4b5a278694ac";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/F7D3-3FFA";
       fsType = "vfat";
     };
 
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.bnep0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
