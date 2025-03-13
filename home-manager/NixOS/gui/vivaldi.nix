b3e9164a8ac89883e254ef45be5289272e3b1dd4
Yus314 <shizhaoyoujie@gmail.com>
2025-03-13 22:04:57 +0900

    NixOSのための修正

--- home-manager/NixOS/gui/vivaldi.nix
+++ home-manager/NixOS/gui/vivaldi.nix
@@ -1,11 +1,6 @@
 { unstable, ... }:
 {
   programs.vivaldi = {
-<<<<<<< HEAD
-    package = unstable.legacyPackages.x86_64-linux.vivaldi;
     enable = false;
-=======
-    enable = true;
->>>>>>> 6971f88731a88dbeae23c3a7526a3203563003fc
   };
 }
