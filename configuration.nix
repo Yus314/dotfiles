c07041591d3f0d064a976953649925765593307a
Yus314 <shizhaoyoujie@gmail.com>
2025-04-24 22:32:39 +0900

    使用していない設定を削除した。

--- configuration.nix
+++ configuration.nix
@@ -119,12 +119,6 @@
   # /boot がいっぱいになったので保存する履歴を制限
   boot.loader.systemd-boot.configurationLimit = 32;
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -157,8 +151,6 @@
   home-manager = {
     users.kaki = {
       imports = [
-        #   ./home-manager/NixOS/gui/packages.nix
-        #  ./home-manager/NixOS/cli
         ./home-manager/common
         ./home-manager/NixOS/gui
         ./home-manager/NixOS/cli
@@ -169,7 +161,6 @@
         stateVersion = "24.11";
       };
       nixpkgs.config.allowUnfree = true;
-      nixpkgs.config.permittedInsecurePacakges = [ "adobe-reader-9.5.5" ];
       nixpkgs.overlays = [ emacs-overlay.overlays.emacs ];
     };
     backupFileExtension = "hm-backup";
@@ -202,10 +193,7 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     firefox
     mu
