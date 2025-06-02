e78ba9e107ef64a795ea8af23ebf32bc37fbba56
Yus314 <shizhaoyoujie@gmail.com>
2025-06-02 12:52:01 +0900

    hkkb + sops

--- home-manager/NixOS/gui/hyprland.nix
+++ home-manager/NixOS/gui/hyprland.nix
@@ -12,7 +12,7 @@
                                                         	env = __GLX_VENDOR_LIBRARY_NAME,nvidia-drm
       monitor=DVI-D-1,1920x1080@60.00000,0x0,1
       monitor=HDMI-A-1,1920x1080@74.97300,1920x0,1
-      exec-once = chmod 777 ~/dotfiles/shingeta.sh && echo "OK2" >> ~/shingetalog.txt &&  bash ~/dotfiles/shingeta.sh
+      exec-once =  xremap home/kaki/.config/xremap/config.yaml 2>&1 >  home/kaki/shingeta.txt
             exec-once = waybar
     '';
 
@@ -64,5 +64,8 @@
         disable_logs = false;
       };
     };
+  };
+  xdg.configFile = {
+      "xremap/config.yaml".source = ../../../hhkb.yaml;
   };
 }
