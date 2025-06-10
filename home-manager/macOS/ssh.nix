ee1ffb7df7bd1de39d2b7e0a6ae9b6c3d58376cc
Yus314 <shizhaoyoujie@gmail.com>
2025-06-10 20:09:09 +0900

    sshのcloudflareのパスがハードコードされていたのでやめる

--- home-manager/macOS/ssh.nix
+++ home-manager/macOS/ssh.nix
@@ -1,11 +1,11 @@
-{
+{pkgs,...}:{
   programs.ssh = {
     enable = true; 
     matchBlocks = {
       "127.0.0.53" = {
         user = "kaki";
 	port = 53;
-        proxyCommand = "/Users/kotsu/.nix-profile/bin/cloudflared access ssh --hostname test.mdip2home.com";
+        proxyCommand = "/{pkgs.cloudflare} access ssh --hostname test.mdip2home.com";
         identityFile = [
           "~/.ssh/id_ed25519"
         ];
