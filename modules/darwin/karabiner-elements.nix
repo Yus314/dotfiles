263b2f69db41a84bcfd3cdf4113b290531cb307e
Yus314 <shizhaoyoujie@gmail.com>
2025-06-14 18:54:04 +0900

    darwinのmoduleを追加

--- modules/darwin/karabiner-elements.nix
+++ modules/darwin/karabiner-elements.nix
@@ -1,6 +1,11 @@
 # modules/karabiner-elements.nix
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   # cfgは config.services.karabiner-elements への短いエイリアス
@@ -25,7 +30,7 @@ in
       description = "Path to the karabiner.edn file used by Goku.";
       # 例: gokuConfigPath = ./karabiner.edn;
     };
-    
+
     # 関連パッケージのオプション
     package = lib.mkOption {
       type = lib.types.package;
@@ -48,35 +53,31 @@ in
 
     # Gokuを使用する場合のロジック
     # lib.mkIf を使って useGoku が true の場合のみ実行
-    config = lib.mkIf cfg.useGoku {
-      let
-        # EDNファイルから karabiner.json をビルドするDerivation
-        karabinerJson = pkgs.stdenv.mkDerivation {
-          pname = "karabiner-config-from-goku";
-          version = "local";
-          src = cfg.gokuConfigPath; # ユーザーが指定したEDNファイルパスを使用
-          nativeBuildInputs = [ cfg.gokuPackage ];
-          
-          buildPhase = ''
-            export HOME=$(mktemp -d)
-            mkdir -p $HOME/.config/karabiner
-            cp ${src} $HOME/.config/karabiner/karabiner.edn
-            goku
-          '';
-          installPhase = ''
-            local generated_json="$HOME/.config/karabiner/karabiner.json"
-            mkdir -p $out
-            cp "$generated_json" $out/karabiner.json
-          '';
-        };
-      in
-      {
-        # Home Managerで、ビルドしたファイルをホームディレクトリの適切な場所に配置
-        home.file.".config/karabiner/karabiner.json" = {
-          source = "${karabinerJson}/karabiner.json";
-          # この設定により、gokuConfigPathで指定したEDNファイルが変更されると、
-          # 自動的にJSONが再ビルドされ、シンボリックリンクが更新される
-        };
+    config = lib.mkIf cfg.useGoku rec {
+      # Home Managerで、ビルドしたファイルをホームディレクトリの適切な場所に配置
+      # EDNファイルから karabiner.json をビルドするDerivation
+      karabinerJson = pkgs.stdenv.mkDerivation rec {
+        pname = "karabiner-config-from-goku";
+        version = "local";
+        src = cfg.gokuConfigPath; # ユーザーが指定したEDNファイルパスを使用
+        nativeBuildInputs = [ cfg.gokuPackage ];
+
+        buildPhase = ''
+          export HOME=$(mktemp -d)
+          mkdir -p $HOME/.config/karabiner
+          cp ${src} $HOME/.config/karabiner/karabiner.edn
+          goku
+        '';
+        installPhase = ''
+          local generated_json="$HOME/.config/karabiner/karabiner.json"
+          mkdir -p $out
+          cp "$generated_json" $out/karabiner.json
+        '';
+      };
+      home.file.".config/karabiner/karabiner.json" = {
+        source = "${karabinerJson}/karabiner.json";
+        # この設定により、gokuConfigPathで指定したEDNファイルが変更されると、
+        # 自動的にJSONが再ビルドされ、シンボリックリンクが更新される
       };
     };
   };
