67dad5eda3db892aaa46d0e9a46a24242fdd1a3f
Yus314 <shizhaoyoujie@gmail.com>
2025-06-17 08:21:40 -0700

    macでsopsが有効になるようにした

--- systems/darwin/common.nix
+++ systems/darwin/common.nix
@@ -1,9 +1,31 @@
-{specialArgs,lib,...}:let
+{specialArgs,lib,inputs,config,...}:let
   inherit (specialArgs) username;
 in{
-  imports = [../../modules/darwin ../common.nix];
+  imports = [../../modules/darwin ../common.nix inputs.sops-nix.darwinModules.sops];
   system.stateVersion = 6;
-
+  sops = {
+    defaultSopsFile = ../../secrets/default.yaml;
+    age = {
+      keyFile = "/Users/kaki/.config/sops/age/keys.txt";
+      #generateKey = true;
+    };
+    secrets = {
+      gh-token = { };
+    };
+    templates = {
+      "gh-token" = {
+        owner = "kaki";
+        #group = "users";
+        mode = "0440";
+        content = ''
+          	  access-tokens = github.com=${config.sops.placeholder."gh-token"}
+          	'';
+      };
+    };
+  };
+    nix.extraOptions = ''
+      !include ${config.sops.templates."gh-token".path}
+    '';
     users = {
     users.${username} = {
       home = "/Users/${username}";
