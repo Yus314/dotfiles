1e70b068437ab679b4494f4902756c8bd3d75f98
Yus314 <shizhaoyoujie@gmail.com>
2025-06-19 12:29:32 -0700

    gitでsignできるようにする

--- applications/git/default.nix
+++ applications/git/default.nix
@@ -4,13 +4,17 @@
     enable = true;
     userName = "Yus314";
     userEmail = "shizhaoyoujie@gmail.com";
+    signing = {
+      key = "B0F6B192E4253A5DBD33A7F2D2A9353AEDF9200A";
+      format = "openpgp";
+      signByDefault = true;
+    };
     extraConfig = {
       core = {
         editor = "nvim";
       };
       pull.rebase = false;
     };
-    signing.format = "ssh";
     ignores = [
       ".DS_Store"
       ".direnv"
