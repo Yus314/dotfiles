b2e5c18934b84a33cf44213d3c8977f4d0833cfd
Yus314 <shizhaoyoujie@gmail.com>
2025-06-20 01:29:35 -0700

    IDを変更

--- applications/git/default.nix
+++ applications/git/default.nix
@@ -5,7 +5,7 @@
     userName = "Yus314";
     userEmail = "shizhaoyoujie@gmail.com";
     signing = {
-      key = "3FF515779AAC5B11BAE41C00A9CD106F20CB7E85";
+      key = "A9CD106F20CB7E85";
       format = "openpgp";
       signByDefault = true;
     };
