4b98fa8a9d989215523f1e9db57587c0f58d3661
Yus314 <shizhaoyoujie@gmail.com>
2025-08-07 01:12:38 +0900

    style(applications/hyprland): fix trailing whitespace in smart-move scripts

--- applications/hyprland/smart-move.nix
+++ applications/hyprland/smart-move.nix
@@ -3,7 +3,7 @@
 {
   hyprland-smart-move-left = pkgs.writeShellScriptBin "hyprland-smart-move-left" ''
     current_ws=$(${pkgs.hyprland}/bin/hyprctl activeworkspace | grep "workspace ID" | cut -d' ' -f3)
-    
+
     if ${pkgs.hyprland}/bin/hyprctl activewindow | grep -q "Invalid"; then
       # No active window: move to previous workspace
       target_ws=$((current_ws > 1 ? current_ws - 1 : 1))
@@ -16,7 +16,7 @@
 
   hyprland-smart-move-right = pkgs.writeShellScriptBin "hyprland-smart-move-right" ''
     current_ws=$(${pkgs.hyprland}/bin/hyprctl activeworkspace | grep "workspace ID" | cut -d' ' -f3)
-    
+
     if ${pkgs.hyprland}/bin/hyprctl activewindow | grep -q "Invalid"; then
       # No active window: move to next workspace
       target_ws=$((current_ws < 9 ? current_ws + 1 : 9))
