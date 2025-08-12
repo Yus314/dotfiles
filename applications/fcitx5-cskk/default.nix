06f2656e05df9b07ccc8a15c031f73e1ce3cbade
Yus314 <shizhaoyoujie@gmail.com>
2025-08-12 11:11:40 +0900

    style(applications/fcitx5-cskk): fix formatting and dictionary order
    - Place user dictionary before system dictionary in list
    - Fix whitespace and formatting for consistency
    - Add newline at end of file

--- applications/fcitx5-cskk/default.nix
+++ applications/fcitx5-cskk/default.nix
@@ -7,16 +7,16 @@
 {
   # Create dictionary_list configuration file
   xdg.dataFile."fcitx5/cskk/dictionary_list".text = ''
-    type=file,file=${config.xdg.dataHome}/fcitx5/cskk/dictionary/SKK-JISYO.L,mode=readonly,encoding=euc-jp,complete=false
     type=file,file=${config.xdg.dataHome}/fcitx5/cskk/user.dict,mode=readwrite,encoding=utf-8,complete=false
+    type=file,file=${config.xdg.dataHome}/fcitx5/cskk/dictionary/SKK-JISYO.L,mode=readonly,encoding=euc-jp,complete=false
   '';
 
   # Link system dictionary from Nix store
-  xdg.dataFile."fcitx5/cskk/dictionary/SKK-JISYO.L".source = 
+  xdg.dataFile."fcitx5/cskk/dictionary/SKK-JISYO.L".source =
     "${pkgs.skkDictionaries.l}/share/skk/SKK-JISYO.L";
 
   # Initialize user dictionary file if it doesn't exist
-  home.activation.fcitx5CskkSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  home.activation.fcitx5CskkSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -f "${config.xdg.dataHome}/fcitx5/cskk/user.dict" ]; then
       $DRY_RUN_CMD mkdir -p "${config.xdg.dataHome}/fcitx5/cskk"
       $DRY_RUN_CMD touch "${config.xdg.dataHome}/fcitx5/cskk/user.dict"
