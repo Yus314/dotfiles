75b04eaee390c87f5b000ce10bfe469e76abbb90
Yus314 <shizhaoyoujie@gmail.com>
2025-08-18 12:47:27 +0900

    fix(common): disable common user shell

--- systems/common.nix
+++ systems/common.nix
@@ -27,7 +27,7 @@ in
 
   environment.shells = [ pkgs.fish ];
   programs.fish.enable = true;
-  users.users.${username}.shell = pkgs.fish;
+  #users.users.${username}.shell = pkgs.fish;
 
   nix.gc = {
     automatic = true;
