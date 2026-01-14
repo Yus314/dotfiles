d4f425841ac1830736c78ea75052140a938e72d4
Yus314 <shizhaoyoujie@gmail.com>
2026-01-15 01:08:24 +0900

    feat(applications): add Modus theme and darkman integration for kitty
    - Add Modus_Vivendi theme and remote control settings to kitty
    - Configure darkman with location-based auto dark/light switching
    - Add kitty theme switching scripts to darkman for Modus_Vivendi/Operandi
    - Add lightModeScripts to darkman for proper light mode support

--- applications/darkman/default.nix
+++ applications/darkman/default.nix
@@ -2,10 +2,32 @@
 {
   services.darkman = {
     enable = true;
+    settings = {
+      lat = 35.68;
+      lng = 139.65;
+    };
     darkModeScripts = {
       gtk-theme = ''
-                ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-        	${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
+        ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+        ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
+      '';
+      kitty-theme = ''
+        for socket in /tmp/kitty-*; do
+          [ -S "$socket" ] && ${pkgs.kitty}/bin/kitty @ --to "unix:$socket" set-colors --all --configured \
+            ${pkgs.kitty-themes}/share/kitty-themes/themes/Modus_Vivendi.conf
+        done
+      '';
+    };
+    lightModeScripts = {
+      gtk-theme = ''
+        ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+        ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita'"
+      '';
+      kitty-theme = ''
+        for socket in /tmp/kitty-*; do
+          [ -S "$socket" ] && ${pkgs.kitty}/bin/kitty @ --to "unix:$socket" set-colors --all --configured \
+            ${pkgs.kitty-themes}/share/kitty-themes/themes/Modus_Operandi.conf
+        done
       '';
     };
   };
--- applications/kitty/default.nix
+++ applications/kitty/default.nix
@@ -1,9 +1,20 @@
 {
   programs.kitty = {
     enable = true;
+    font = {
+      name = "Bizin Gothic Discord NF";
+      size = 18;
+    };
+    themeFile = "Modus_Vivendi";
     settings = {
       macos_option_as_alt = "left";
+      allow_remote_control = "socket-only";
+      listen_on = "unix:/tmp/kitty";
+    };
+    shellIntegration = {
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
     };
   };
-
 }
