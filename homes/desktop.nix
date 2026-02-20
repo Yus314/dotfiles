4898b8b4374e57292bb49c238d4bdcb3f644363a
Yus314 <shizhaoyoujie@gmail.com>
2026-02-20 17:16:43 +0900

    chore(homes): disable calibre (marked as broken)
    calibre 8.16.2 is marked as broken upstream; temporarily comment out
    to unblock builds.

--- homes/desktop.nix
+++ homes/desktop.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   imports = [
-    ../applications/calibre
+    # ../applications/calibre  # temporarily disabled: calibre 8.16.2 marked as broken
     ../applications/alacritty
     ../applications/kitty
     ../applications/ghostty
