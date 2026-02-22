2fc1a1704cf47aa75c51afeb3b8af9567fa6741f
Yus314 <shizhaoyoujie@gmail.com>
2026-02-22 19:50:13 +0900

    kitty: default to splits layout

--- applications/kitty/default.nix
+++ applications/kitty/default.nix
@@ -15,6 +15,7 @@
       macos_option_as_alt = "left";
       allow_remote_control = "socket-only";
       listen_on = "unix:/tmp/kitty";
+      enabled_layouts = "splits:split_axis=horizontal";
       # Nerd Font symbols mapping (v3.0+)
       symbol_map = "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+ed00-U+efc1,U+f000-U+f2ff,U+f0001-U+f1af0,U+f300-U+f372,U+f400-U+f533,U+f500-U+fd46 Symbols Nerd Font Mono";
       tab_bar_edge = "top";
