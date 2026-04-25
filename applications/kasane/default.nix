311db81cbfe67b7890c30d5a5a1da0e5a190290e
Yus314 <shizhaoyoujie@gmail.com>
2026-04-26 06:52:04 +0900

    feat(applications/kasane): enable search dropdown and additional plugins

--- applications/kasane/default.nix
+++ applications/kasane/default.nix
@@ -7,5 +7,9 @@
     family = "Bizin Gothic NF"
     size = 24.0
     fallback_list = ["Noto Sans CJK JP", "Noto Color Emoji"]
+    [search]
+    dropdown = true
+    [plugins]
+    enabled = ["cursor_line", "color_preview", "sel_badge", "fuzzy_finder", "image_preview"]
   '';
 }
