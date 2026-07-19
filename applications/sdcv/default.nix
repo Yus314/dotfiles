0c59428bf182f6e727c09be2b753f13e5f199340
Yus314 <shizhaoyoujie@gmail.com>
2026-07-19 20:00:18 +0900

    refactor(sdcv): store dictionaries under XDG data

--- applications/sdcv/default.nix
+++ applications/sdcv/default.nix
@@ -21,11 +21,30 @@ in
 
   # 3. フォルダごと配置
   # "stardict-kojien6-2.4.2" フォルダの中身（.ifo, .idx, .dict.dz 等）をすべて利用可能にします
-  home.file.".stardict/dic/kojien6".source = "${sdcv-repo}/stardict-kojien6-2.4.2";
-  home.file.".stardict/dic/jmdict-en-ja".source = "${sdcv-repo}/stardict-jmdict-en-ja-2.4.2";
-  home.file.".stardict/dic/jmdict-ja-en".source = "${sdcv-repo}/stardict-jmdict-ja-en-2.4.2";
-  home.file.".stardict/dic/edict".source = "${sdcv-repo}/stardict-edict-2.4.2";
-  home.file.".stardict/dic/longman5".source =
-    "${sdcv-repo}/stardict-Longman_Dictionary_of_Contemporary_English_5th_Ed-2.4.2";
-  home.file.".stardict/dic/collins5".source = "${sdcv-repo}/stardict-collins5-2.4.2";
+  xdg.dataFile = {
+    "stardict/dic/kojien6" = {
+      source = "${sdcv-repo}/stardict-kojien6-2.4.2";
+      force = true;
+    };
+    "stardict/dic/jmdict-en-ja" = {
+      source = "${sdcv-repo}/stardict-jmdict-en-ja-2.4.2";
+      force = true;
+    };
+    "stardict/dic/jmdict-ja-en" = {
+      source = "${sdcv-repo}/stardict-jmdict-ja-en-2.4.2";
+      force = true;
+    };
+    "stardict/dic/edict" = {
+      source = "${sdcv-repo}/stardict-edict-2.4.2";
+      force = true;
+    };
+    "stardict/dic/longman5" = {
+      source = "${sdcv-repo}/stardict-Longman_Dictionary_of_Contemporary_English_5th_Ed-2.4.2";
+      force = true;
+    };
+    "stardict/dic/collins5" = {
+      source = "${sdcv-repo}/stardict-collins5-2.4.2";
+      force = true;
+    };
+  };
 }
