749b45d7f9a1197d1a512d9f45a05376dda2f3da
Yus314 <shizhaoyoujie@gmail.com>
2026-08-30 12:37:22 +0900

    fix(bat): make darkman sole config owner

--- applications/bat/default.nix
+++ applications/bat/default.nix
@@ -4,20 +4,26 @@
   pkgs,
   ...
 }:
+let
+  ghosttySyntaxMap = "--map-syntax=${lib.escapeShellArg "${config.xdg.configHome}/ghostty/config:Ghostty Config"}";
+in
 {
   programs.bat = {
     enable = true;
+    config = lib.mkForce { };
     themes = { };
   };
 
-  xdg.configFile."bat/config.light".text = "--theme=OneHalfLight";
-  xdg.configFile."bat/config.dark".text = "--theme=OneHalfDark";
-
-  home.activation.batCleanup = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-    $DRY_RUN_CMD rm -f "${config.xdg.configHome}/bat/config"
+  xdg.configFile."bat/config.light".text = ''
+    --theme=OneHalfLight
+    ${ghosttySyntaxMap}
+  '';
+  xdg.configFile."bat/config.dark".text = ''
+    --theme=OneHalfDark
+    ${ghosttySyntaxMap}
   '';
 
-  home.activation.batLink = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation.batLink = lib.hm.dag.entryAfter [ "batCache" ] ''
     $DRY_RUN_CMD ${pkgs.coreutils}/bin/ln -sf \
       "${config.xdg.configHome}/bat/config.light" \
       "${config.xdg.configHome}/bat/config"
